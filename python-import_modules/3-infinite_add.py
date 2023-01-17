1e36063ade0402290185c3ebe7c230ab8bdf4160
Mikalach <mika.djetic@gmaiul.com>
2023-01-17 06:18:56 -0800

    test on checker

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import sys
-
-def main():
-    add = sum(int(arg) for arg in sys.argv[1:])
-    print("{}".format(add))
-
-    if name == "main":
-        main()
+if __name__ == "__main__":
+    def main():
+        add = sum(int(arg) for arg in sys.argv[1:])
+        print("{}".format(add))
+        if name == "main":
+            main()
