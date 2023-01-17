4f507b248032b097443a00845c280674d104a66f
Mikalach <mika.djetic@gmaiul.com>
2023-01-17 02:01:19 -0800

    test checker

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,16 +3,5 @@ def print_last_digit(number):
 
     lastDigit = int(str(abs(number))[-1])
 
-    if number < 0:
-        print("Last digit of " + str(number) + " is -" +
-              str(lastDigit) + " and is less than 6 and not 0")
-    elif lastDigit < 6 and lastDigit != 0:
-        print("Last digit of " + str(number) + " is " + str(lastDigit) +
-              " and is less than 6 and not 0")
-    elif lastDigit > 5:
-        print("Last digit of " + str(number) + " is " + str(lastDigit) +
-              " and is greater than 5")
-    else:
-        print("Last digit of " + str(number) + " is " + str(lastDigit) +
-              " and is 0")
+        print(str(lastDigit))
         return lastDigit
