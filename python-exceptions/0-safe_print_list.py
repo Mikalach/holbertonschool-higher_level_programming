d54130c90b94233a90e9f8bdb29712008a964af4
Mikalach <mika.djetic@gmaiul.com>
2023-01-24 08:52:14 -0800

    space removed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     elements = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             elements += 1
     except:
         pass
