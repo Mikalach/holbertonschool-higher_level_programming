c2bf763d179dec6b97adcb99a0aedcf68354b2ba
Mikalach <mika.djetic@gmaiul.com>
2023-01-24 08:18:13 -0800

    added the none case

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is none:
+        return none
     romanDic = {
         "I": 1,
         "V": 5,
@@ -12,7 +14,8 @@ def roman_to_int(roman_string):
     result = 0
     for i in range(len(roman_string)):
         current = romanDic[roman_string[i]]
-        if i + 1 < len(roman_string) and romanDic[roman_string[i + 1]] > current:
+        if i + 1 < len(roman_string) and romanDic[roman_string[i + 1]] >
+        current:
             result -= current
         else:
             result += current
