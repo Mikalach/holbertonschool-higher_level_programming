720bb8ed00ac4b3496d4495c9474270f76cadc8b
Mikalach <mika.djetic@gmaiul.com>
2023-01-27 05:17:45 -0800

    removed the main 4 for the test

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,9 +10,3 @@ def safe_print_list_integers(my_list=[], x=0):
             break
     print()
     return count
-
-
-my_list = [1, 2, 3, 4]
-x = len(my_list) + 4
-nb_print = safe_print_list_integers(my_list, x)
-print("{:d}".format(nb_print))
