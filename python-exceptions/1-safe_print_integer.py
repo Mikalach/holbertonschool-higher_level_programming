3a129f56c543e22c54143439a2fa74184fdc864c
Mikalach <mika.djetic@gmaiul.com>
2023-01-27 02:25:38 -0800

    return to the start

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        int_value = int(value)
-        print("{:d}".format(int_value))
+        print("{:d}".format(int(value)))
         return True
-    except ValueError:
+    except:
         return False
