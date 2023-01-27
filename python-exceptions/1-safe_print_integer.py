2f4f0a28eeea5467156517ed86fdbb3d3a72dbcc
Mikalach <mika.djetic@gmaiul.com>
2023-01-27 02:22:56 -0800

    changed the order

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        int_value = int(value)
+        print("{:d}".format(int_value))
+        return True
     except ValueError:
         return False
-    except:
-        return False
-    return True
