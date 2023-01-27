efa77b5c4effc828d029643b971a9ac936752410
Mikalach <mika.djetic@gmaiul.com>
2023-01-27 02:19:46 -0800

    valueerror added

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-        return True
     except ValueError:
+        return False
     except:
         return False
+    return True
