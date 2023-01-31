cbfe5eb6685a20f7b10076df6e68ae1e19c81a77
Mikalach <mika.djetic@gmaiul.com>
2023-01-31 01:15:39 -0800

    indent problem

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -13,27 +13,27 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        @property
-        def size(self):
-            return (self.__size)
+    @property
+    def size(self):
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """ Return the square area """
-            return (self.__size)**2
+    def area(self):
+        """ Return the square area """
+        return (self.__size)**2
 
-        def my_print(self):
-            """ Print a square of size size"""
-            if (self.__size == 0):
-                print()
-                for i in range(self.__size):
-                    for j in range(self.__size):
-                        print("#", end='')
-                        print()
+    def my_print(self):
+        """ Print a square of size size"""
+        if (self.__size == 0):
+            print()
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                    print()
