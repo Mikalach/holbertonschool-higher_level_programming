29d3d632e31f87fe1202b65f28e0962991fa0807
Mikalach <mika.djetic@gmaiul.com>
2023-01-31 00:55:09 -0800

    val movement

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,11 +7,11 @@ class Square:
     """Empty class"""
     def __init__(self, size=0):
         """initialisation of the size, with some constraint"""
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
 
         def area(self):
             """return the square area"""
