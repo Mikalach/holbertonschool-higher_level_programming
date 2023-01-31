3a0ce0d399d4325df93a9408d34205683dfb0d62
Mikalach <mika.djetic@gmaiul.com>
2023-01-31 01:02:30 -0800

    indent problem

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -25,6 +25,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-            def area(self):
-                """return the square area"""
-                return (self.__size)**2
+    def area(self):
+        """return the square area"""
+        return (self.__size)**2
