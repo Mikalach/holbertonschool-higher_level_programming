1b19c29787ddb3f1147aa20e0e12381cd2707465
Mikalach <mika.djetic@gmaiul.com>
2023-02-01 01:59:49 -0800

    changed size with height and width

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle():
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = size
+        self.__width = value
 
     @property
     def height(self):
@@ -31,4 +31,4 @@ class Rectangle():
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = size
+        self.__height = value
