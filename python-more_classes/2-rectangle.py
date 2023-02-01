8376fde271df52baa972e9c4ae919418220b72e5
Mikalach <mika.djetic@gmaiul.com>
2023-02-01 03:58:01 -0800

    added perim and area

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -34,3 +34,12 @@ class Rectangle():
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width * self.__height)
