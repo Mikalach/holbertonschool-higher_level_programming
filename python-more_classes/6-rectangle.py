706f71e5bf2f82e70dd4846fbf16967cc1005fe9
Mikalach <mika.djetic@gmaiul.com>
2023-02-02 02:34:29 -0800

    peps

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle():
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        Rectangle.number_of_instances +=1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
