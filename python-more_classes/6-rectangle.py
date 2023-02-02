ab6fca7f708e49b0b963592b14acb635318caedc
Mikalach <mika.djetic@gmaiul.com>
2023-02-02 02:28:43 -0800

    gab helped with self removing

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -11,6 +11,7 @@ class Rectangle():
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
+        Rectangle.number_of_instance +=1
 
     @property
     def width(self):
