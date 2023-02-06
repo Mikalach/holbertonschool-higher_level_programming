1bc10e4818a2949bbb94aa20f9979362762d3ee7
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 07:41:53 -0800

    back again

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,7 +2,23 @@
 """ module documentation """
 
 
-BaseGeometry = __import__("9-base_geometry").BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class rectangel """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self._height = height
+
+    def area(self):
+        return self._width * self._height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self._width, self._height)
 
 class Square(Rectangle):
     """ Class Square """
