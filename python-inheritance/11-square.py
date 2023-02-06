e6007dd9c856a64826db4e2579cbbb234f9a00a3
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 07:21:24 -0800

    started it without wanting

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" module documentation """
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class rectangel """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self._height = height
+
+    def area(self):
+        return self._width * self._height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self._width, self._height)
+
+
+class Square(Rectangle):
+    """ Class Square """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
