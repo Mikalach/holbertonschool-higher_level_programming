65bbaa7261c996ae03f2582a31129eca9b86d9e6
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 02:14:43 -0800

    start task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" module documented """
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance of the specified class; otherwise False.
+
+    Args:
+    obj (object): The object to inspect.
+    a_class (type): The class to compare with.
+
+    Returns:
+    bool: True if the object is exactly an instance of the specified class; otherwise False.
+    """
+    return type(obj) is a_classdef is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance of the specified class; otherwise False.
+
+    Args:
+    obj (object): The object to inspect.
+    a_class (type): The class to compare with.
+
+    Returns:
+    bool: True if the object is exactly an instance of the specified class; otherwise False.
+    """
+    return type(obj) is a_class
