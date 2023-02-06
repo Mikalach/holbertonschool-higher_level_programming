7c65f5cfb029bb38b572c6251d4a7528cfac3f75
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 03:58:36 -0800

    removed type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -18,4 +18,4 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class; otherwise False.
     """
 
-    return isinstance(type(obj), a_class)
+    return isinstance(obj, a_class)
