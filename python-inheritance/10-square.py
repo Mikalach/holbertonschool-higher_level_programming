18e0fa5da5e2f88cd5fb456e5759192b8d1ca506
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 07:40:10 -0800

    test without rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,24 +2,7 @@
 """ module documentation """
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """ Class rectangel """
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self._width = width
-        self._height = height
-
-    def area(self):
-        return self._width * self._height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self._width, self._height)
-
+BaseGeometry = __import__("9-base_geometry").BaseGeometry
 
 class Square(Rectangle):
     """ Class Square """
