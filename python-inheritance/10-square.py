6223137cd669c93d8df5fefcf5e4c51c9d7c0cc3
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 07:04:17 -0800

    changed the import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,7 +2,7 @@
 """ module documentation """
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__("9-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
