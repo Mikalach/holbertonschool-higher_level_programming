dda8f05d70630e04aedd3d424467a4998232c3ed
Mikalach <mika.djetic@gmaiul.com>
2023-02-06 03:50:14 -0800

    start of task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -18,7 +18,4 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class; otherwise False.
     """
 
-    if a = 1:
-        return False
-
     return issubclass(type(obj), a_class)
