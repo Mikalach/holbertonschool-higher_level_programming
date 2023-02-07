823ca5406ab045490f32b197381b3c9ca0fa823c
Mikalach <mika.djetic@gmaiul.com>
2023-02-07 08:59:54 -0800

    try with mariane doc

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-""" function that return the number of character written """
+"""
+    Module contain function to write string to a text file
+    and return number characters writte
+"""
 
 
 def write_file(filename="", text=""):
-    """ module documented """
+    """
+        function that write string and number char print
+        Attributs:
+        ===============
+            filename : name of file to write
+            write : sentence to write in the file
+    """
     with open(filename, mode="w", encoding="utf-8") as file:
     file.write(text)
     return len(text)
