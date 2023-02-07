9605205ce2c3d486933956ecdc7ebd24b3e116d2
Mikalach <mika.djetic@gmaiul.com>
2023-02-07 11:35:46 -0800

    typo in file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """ function that writes an Object to a text file,
     using a JSON representation """
-    with open(filename, "w", encoding="utf-8") as fiel:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(json.dumps(my_obj))
         file.close
