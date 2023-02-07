192421a4425bfc61308d00129a100e9d539ab5dd
Mikalach <mika.djetic@gmaiul.com>
2023-02-07 00:42:02 -0800

    imported 9

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,11 +9,14 @@ class Square(Rectangle):
     """ Class Square """
 
     def __init__(self, size):
+        """ comment """
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """ comment """
         return self.__size * self.__size
 
     def __str__(self):
+        """ comment """
         return "[Square] {}/{}".format(self.__size, self.__size)
