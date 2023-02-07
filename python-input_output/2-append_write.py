45714e49b363d285a083d66b131783f845c7b1a7
Mikalach <mika.djetic@gmaiul.com>
2023-02-07 10:41:13 -0800

    line shortened

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,7 +4,8 @@
 
 def append_write(filename="", text=""):
     """
-    This function appends a string to the end of a text file and returns the number of characters added.
+    This function appends a string to the end of a text file and returns
+    the number of characters added.
     If the file doesn't exist, it creates the file.
     """
     with open(filename, 'a') as file:
