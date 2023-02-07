9348e2c20255bf0352514db6a7e06e84eec3fa98
Mikalach <mika.djetic@gmaiul.com>
2023-02-07 08:53:16 -0800

    start of task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that return the number of character written """
+
+
+def write_file(filename="", text=""):
+    """ module documented """
+    with open(filename, mode="w", encoding="utf-8") as file:
+    file.write(text)
+    return len(text)
