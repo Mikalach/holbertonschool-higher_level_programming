ae16bf6edc0c0905b1b80ecaa537daad2b608778
Mikalach <mika.djetic@gmaiul.com>
2023-02-09 00:39:36 -0800

    moduled

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ module documented """
+
+
 if __name__ == '__main__':
-import sys
-import json
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+    import sys
+    import json
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
 
-
-    """ script that adds all arguments to a Python list, and then save
-    them to a file """
     try:
         data = load_from_json_file("add_item.json")
     except:
