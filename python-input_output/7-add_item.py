188f48149a08cf7195d101b82c192473d671bb1a
Mikalach <mika.djetic@gmaiul.com>
2023-02-09 00:35:56 -0800

    gab said it works no test*

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ module documented """
+if __name__ == '__main__':
 import sys
 import json
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-def add_item(args):
+
     """ script that adds all arguments to a Python list, and then save
     them to a file """
     try:
@@ -17,6 +18,3 @@ def add_item(args):
         data.append(arg)
 
     save_to_json_file(data, "add_item.json")
-
-if __name__ == '__main__':
-    add_item(sys.argv[1:])
