a72f29df607a7afbdc393ac54f2c1d88d8b6feae
Mikalach <mika.djetic@gmaiul.com>
2023-02-13 13:34:02 -0800

    wrong file so i traded

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,8 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Return a dictionary representation of a Rectangle instance."""
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
