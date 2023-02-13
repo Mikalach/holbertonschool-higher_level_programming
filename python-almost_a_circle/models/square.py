00a7ea507c1ac1fcffb5a662411388212d9baf9d
Mikalach <mika.djetic@gmaiul.com>
2023-02-13 09:14:27 -0800

    testo

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,3 +7,11 @@ class Square(Rectangle):
     """ class Square that inherits from Rectangle """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        repsqr = "[Square] ({}) {}/{} - {}"
+        id = self.id
+        x = self.x
+        y = self.y
+        size = self.width
+        return repsqr.format(id, x, y, size)
