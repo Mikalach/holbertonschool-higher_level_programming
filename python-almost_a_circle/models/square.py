f19809784c6d6ac350fc0e959de6eb6e84255b13
Mikalach <mika.djetic@gmaiul.com>
2023-02-13 09:20:20 -0800

    add property

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -15,3 +15,27 @@ class Square(Rectangle):
         y = self.y
         size = self.width
         return repsqr.format(id, x, y, size)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
