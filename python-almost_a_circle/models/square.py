92204c53ed80d7e4212089fd5970657a93793680
Mikalach <mika.djetic@gmaiul.com>
2023-02-13 12:04:42 -0800

    module bad indent so all bad

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,24 +26,24 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-    """ module documented """
-    if args:
-        self.id = args[0]
-    if len(args) > 1:
-        self.width = args[1]
-        self.height = args[1]
-    if len(args) > 2:
-        self.x = args[2]
-    if len(args) > 3:
-        self.y = args[3]
-    else:
-        for key, value in kwargs.items():
-            if key == "id":
-                self.id = value
-            if key == "size":
-                self.width = value
-                self.height = value
-            if key == "x":
-                self.x = value
-            if key == "y":
-                self.y = value
+        """ module documented """
+        if args:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+            self.height = args[1]
+        if len(args) > 2:
+            self.x = args[2]
+        if len(args) > 3:
+            self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
