3a3a7fd822787e1dcd1a99b17eea8b863677b681
Mikalach <mika.djetic@gmaiul.com>
2023-02-13 04:21:09 -0800

    peps 8

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module documeted """
 
+
 class Base:
     """ This class will be the “base” of all other classes in this project.
     The goal of it is to manage id attribute in all your future classes and
