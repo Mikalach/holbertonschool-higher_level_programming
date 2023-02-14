1aec0afc4c4b944676d9269751bddfdd024d9642
Mikalach <mika.djetic@gmaiul.com>
2023-02-14 07:16:36 -0800

    test 1

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -6,11 +6,7 @@ class TestRectangle(unittest.TestCase):
 
     def test_rectangle_1(self):
         r = Rectangle(1, 2)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 0)
-        self.assertEqual(r.y, 0)
-        self.assertNotEqual(r.id, None)
+        self.assertEqual(r, [Rectangle] (1) 0/0 - 1/2)
 
 
 
