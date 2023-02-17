f0985220a07fe3d513021d5151e427c2a8b97744
Mikalach <mika.djetic@gmaiul.com>
2023-02-17 02:58:58 -0800

    try

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,101 +1,97 @@
-"""import unittest
+#!/usr/bin/python3
+"""test_unittest base
+    """
+import unittest
+import json
+from os.path import exists
+from models.base import Base
 from models.rectangle import Rectangle
 
-class Test_Rectangle(unittest.TestCase):
-    def test_1(self):
-        rect = Rectangle(1, 2)
-        self.assertEqual(rect.id, 7)
-        self.assertEqual(rect.width, 1)
-        self.assertEqual(rect.height, 2)
 
-    def test_2(self):
-        rect2 = Rectangle(1, 2, 3)
-        self.assertEqual(rect2.id, 16)
-        self.assertEqual(rect2.width, 1)
-        self.assertEqual(rect2.height, 2)
-        self.assertEqual(rect2.x, 3)
+class Base_test_class(unittest.TestCase):
+    """class test base
 
-    def test_3(self):
-        rect3 = Rectangle(1, 2, 3, 4)
-        self.assertEqual(rect3.id, 17)
-        self.assertEqual(rect3.width, 1)
-        self.assertEqual(rect3.height, 2)
-        self.assertEqual(rect3.x, 3)
-        self.assertEqual(rect3.y, 4)
+    """
 
-    def test_6(self):
-        rect4 = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(rect4.id, 5)
-        self.assertEqual(rect4.width, 1)
-        self.assertEqual(rect4.height, 2)
-        self.assertEqual(rect4.x, 3)
-        self.assertEqual(rect4.y, 4)
+    def tests_instentiation(self):
+        """test when base have arg and
+        when it's equal to none"""
+        Base.__nb_objects = 0
 
-    def test_7(self):
-        with self.assertRaises(TypeError):
-            Rectangle("1", 2)
-        with self.assertRaises(TypeError):
-            Rectangle(1, "2")
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, "3")
-        with self.assertRaises(TypeError):
-            Rectangle(1, 2, 3, "4")
-        with self.assertRaises(ValueError):
-            Rectangle(-1, 2)
-        with self.assertRaises(ValueError):
-            Rectangle(1, -2)
-        with self.assertRaises(ValueError):
-            Rectangle(0, 2)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 0)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 2, -3)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 2, 3, -4)
-        
-    def test_8(self):
-        rect5 = Rectangle(3, 3)
-        self.assertEqual(rect5.id, 18)
-        self.assertEqual(rect5.width, 3)
-        self.assertEqual(rect5.height, 3)
-        self.assertEqual(rect5.area(), 9)
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-    def test_9(self):
-        rect6 = Rectangle(3, 3, 3, 3)
-        self.assertEqual(rect6.id, 19)
-        self.assertEqual(rect6.width, 3)
-        self.assertEqual(rect6.height, 3)
-        self.assertEqual(rect6.area(), 9)
-        self.assertEqual(rect6.__str__(), "[Rectangle] (19) 3/3 - 3/3")
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
 
-    def test_10(self):
-        rect7 = Rectangle(3, 2, 0, 0)
-        self.assertEqual(rect7.display(), "###\n###\n")
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
 
-    def test_11(self):
-        rect8 = Rectangle(3, 2, 1, 2, 15)
-        self.assertEqual(rect8.to_dictionary(), {'id': 15, 'width': 3, 'height': 2, 'x': 1, 'y': 2})
+        b = Base()
+        self.assertEqual(b.id, 3)
 
-    def test_12(self):
-        rect9 = Rectangle(10, 5, 0, 0, 14)
-        self.assertEqual(rect9.id, 14)
-        rect9.update(id=5)
-        self.assertEqual(rect9.id, 5)
+    def tests_input(self):
+        """test for valid input for positive int or negativ and float
+        """
+        b_pos = Base(12)
+        self.assertEqual(b_pos.id, 12)
 
-    def test_13(self):
-        rect10 = Rectangle.create(**{'id': 89})
-        self.assertEqual(rect10.id, 89)
+        b_neg = Base(-12)
+        self.assertEqual(b_neg.id, -12)
 
-    def test_14(self):
-        recc1 = Rectangle(3, 3, 2, 0, 56)
-        recc2 = Rectangle(4, 4, 5, 3, 15)
-        self.assertEqual(Rectangle.save_to_file([recc1, recc2]), 0)
+        b_float = Base(6.8)
+        self.assertEqual(b_float.id, 6.8)
 
-    def test_15(self):
-        Rectangle.load_from_file()
-        recc1 = Rectangle(3, 3, 4, 0)
-        recc2 = Rectangle(5, 5)
-        list_rectangles_input = [recc1, recc2]
-        Rectangle.save_to_file(list_rectangles_input)
-        list_rectangles_output = Rectangle.load_from_file()
-"""
+    def test_id(self):
+        """ tesd id """
+        self.assertEqual(5, Base(5).id)
+        b, b.id = Base(10), 5
+        self.assertEqual(5, b.id)
+
+    def tests_pub_id(self):
+        """test for public id
+        """
+        b = Base(12)
+        b.id = 21
+        self.assertEqual(21, b.id)
+
+    def tests_str(self):
+        """test string
+        """
+        b_str = Base("school")
+        self.assertEqual(b_str.id, "school")
+
+    def tests_num_private(self):
+        """test for private number object
+        """
+        b = Base(5)
+        with self.assertRaises(AttributeError):
+            print(b.nb__objects)
+
+    def test_base_tuple_id(self):
+        """ test tuple id """
+        self.assertEqual((1, 2), Base((1, 2)).id)
+
+    def tests_json_str(self):
+        Base._Base__nb_objects = 0
+        l1 = {"id": 5, "width": 5, "height": 6, "x": 8, "y": 9}
+        l2 = {"id": 6, "width": 7, "height": 7, "x": 9, "y": 8}
+        new_json = Base.to_json_string([l1, l2])
+        self.assertTrue(type(new_json) is str)
+        list = json.loads(new_json)
+        self.assertEqual(list, [l1, l2])
+
+    def tests_from_json_file(self):
+        """test for normal from json file
+        """
+        str = '[{"id": 5, "width": 5, "height": 6, "x": 8, "y": 9}, \
+            {"id": 6, "width": 7, "height": 7, "x": 9, "y": 8}]'
+        json_file = Base.from_json_string(str)
+        self.assertTrue(type(json_file) is list)
+        self.assertEqual(len(json_file), 2)
+        self.assertTrue(type(json_file[0]) is dict)
+        self.assertTrue(type(json_file[1]) is dict)
+        self.assertEqual(json_file[0],
+                         {"id": 5, "width": 5, "height": 6, "x": 8, "y": 9})
+        self.assertEqual(json_file[1],
+                         {"id": 6, "width": 7, "height": 7, "x": 9, "y": 8})
