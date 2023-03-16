f5c00247c0d1fbe236d897e4801902e5629ed0af
Mikalach <mika.djetic@gmaiul.com>
2023-03-16 12:14:38 -0700

    asked gpt for comments

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the query to fetch all states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows and print them
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
