0852ad02e9a67d730fb84dbcf84e6ca21bc6ac34
Mikalach <mika.djetic@gmaiul.com>
2023-03-16 12:19:30 -0700

    moved the doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """ lists all states from the database hbtn_0e_0_usa """
 
+
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
