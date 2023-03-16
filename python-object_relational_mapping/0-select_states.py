5227a9423235e0bfbdc6129d1ac8526b273d7812
Mikalach <mika.djetic@gmaiul.com>
2023-03-16 12:17:29 -0700

    removed #s

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -14,17 +13,17 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-    # Create a cursor object to interact with the database
+    """ Create a cursor object to interact with the database """
     cur = db.cursor()
 
-    # Execute the query to fetch all states
+    """ Execute the query to fetch all states """
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all rows and print them
+    """ Fetch all rows and print them """
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and database connection
+    """ Close the cursor and database connection """
     cur.close()
     db.close()
