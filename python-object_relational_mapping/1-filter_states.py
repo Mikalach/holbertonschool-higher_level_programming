1d82503a6af59078db074e5f3b23f997b4fa62b5
Mikalach <mika.djetic@gmaiul.com>
2023-03-17 14:42:19 -0700

    changes

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ script that lists all states with a name starting with N (upper N) from
 the database hbtn_0e_0_usa: """
-
-
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
