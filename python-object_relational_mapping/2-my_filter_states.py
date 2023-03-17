f9d03cff787748dc0465e54c8d8cb9a7c9cef9ee
Mikalach <mika.djetic@gmaiul.com>
2023-03-17 10:33:26 -0700

    try module documented

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Write a script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument. """
+""" the database hbtn_0e_0_usa: """
 
 
 import MySQLdb
