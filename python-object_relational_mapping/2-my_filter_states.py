ba4902b86cca0c1c395ed50e71020534c07a08e8
Mikalach <mika.djetic@gmaiul.com>
2023-03-17 10:31:11 -0700

    fraud

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument. """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username, password, database, state_name = sys.argv[1], sys.argv[2],
+    sys.argv[3], sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'
+        ORDER BY states.id ASC".format(state_name)
+    )
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+
+    db.close()
