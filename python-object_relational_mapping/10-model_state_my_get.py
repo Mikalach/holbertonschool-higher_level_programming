a2435951121d33c79761a8bbdc47d0791e676cc1
Mikalach <mika.djetic@gmaiul.com>
2023-03-17 12:08:27 -0700

    forgot things

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Table Print """
-
+""" Module documentend """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,18 +7,15 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Connect to the database
     db_user, db_pass, db_name, db_host = sys.argv[1], sys.argv[2],
     sys.argv[3], "localhost"
     engine = create_engine(f"mysql+mysqldb://
     {db_user}:{db_pass}@{db_host}/{db_name}")
     Base.metadata.create_all(engine)
 
-    # Query the database for the state with the given name
     with sessionmaker(bind=engine)() as session:
         state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-        # Print the ID of the state or "Not found" if no state is found
         if state is not None:
             print(state.id)
         else:
