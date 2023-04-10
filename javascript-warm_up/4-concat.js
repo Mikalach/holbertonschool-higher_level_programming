d3987e6d8fb2f398e00ec3182f36fd23066ea6f9
Mikalach <mika.djetic@gmaiul.com>
2023-04-10 01:50:07 -0700

    prettier code

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const arg2 = process.argv[2];
-const arg3 = process.argv[3];
+const arg = process.argv;
 
-console.log(`${arg2} is ${arg3}`);
+console.log(`${arg[2]} is ${arg[3]}`);
