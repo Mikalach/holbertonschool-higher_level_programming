8691c8df9ff8cfb4ee2cff9aeac0b1888db94e88
Mikalach <mika.djetic@gmaiul.com>
2023-04-10 08:24:29 -0700

    changed import and extend

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Square5 {
   constructor (size) {
     super(size, size);
   }
+
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
