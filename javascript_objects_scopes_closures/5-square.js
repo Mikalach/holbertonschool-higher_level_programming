5ed2afe0a68595f61df83ed4149bd43d8ff675d9
Mikalach <mika.djetic@gmaiul.com>
2023-04-10 08:21:00 -0700

    added the for to print

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -6,6 +6,18 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Square;
