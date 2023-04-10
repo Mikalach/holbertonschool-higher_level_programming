12f8ad276d588e0302898577022c3fd07e246406
Mikalach <mika.djetic@gmaiul.com>
2023-04-10 08:17:11 -0700

    wrong file name in require

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-const Rectangle = require('./rectangle');
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-  }
-
-  toString () {
-    return `[Square] ${this.width} x ${this.height}`;
   }
 }
 
