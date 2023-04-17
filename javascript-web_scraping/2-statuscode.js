d10d827403e89752ee9009853471111b3742007f
Mikalach <mika.djetic@gmaiul.com>
2023-04-17 03:47:57 -0700

    removed the print words of the console log error

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -8,6 +8,6 @@ request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(`code: ${response.statusCode}`);
   } else {
-    console.error(`Error fetching URL: ${error}`);
+    console.error(`${error}`);
   }
 });
