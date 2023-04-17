0b17dc2753dbb61dfd220f8a9b3594a239012983
Mikalach <mika.djetic@gmaiul.com>
2023-04-17 01:59:44 -0700

    semi standard fix to pass the checker

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const filename = process.argv[2];
 
-fs.readFile(filename, 'utf8', function(err, data) {
+fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.error(`Error reading file: ${err}`);
   } else {
