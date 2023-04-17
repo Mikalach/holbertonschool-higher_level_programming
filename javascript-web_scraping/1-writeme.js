d25eaf2c75d1aa55f33c1184a7fd2cd4449ec6f4
Mikalach <mika.djetic@gmaiul.com>
2023-04-17 02:13:03 -0700

    correcting the output inside the print so it doesnt say successfull

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -9,6 +9,6 @@ fs.writeFile(filePath, data, 'utf8', (err) => {
   if (err) {
     console.error(`Error writing to file: ${err}`);
   } else {
-    console.log(`Successfully wrote ${data} to ${filePath}`);
+    console.log(`${data}`);
   }
 });
