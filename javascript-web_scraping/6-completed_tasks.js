5ae0440b77cc63cda8a84c2b936d98607e6003a1
Mikalach <mika.djetic@gmaiul.com>
2023-04-17 07:54:01 -0700

    i start to loose my mind on this task

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
 const request = require('request');
 
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
-
+const apiUrl = argv[2]
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
@@ -23,6 +23,6 @@ request(apiUrl, (error, response, body) => {
       }
     }
   }
-
-  console.log(JSON.stringify(completedTasks, null, 2));
-});
+    console.log(JSON.stringify(completedTasks));
+  }
+);
