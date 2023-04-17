f3007ce63882f765d055454943670a9e4958504c
Mikalach <mika.djetic@gmaiul.com>
2023-04-17 05:03:34 -0700

    forgot to push the task so it did not work

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+request(`https://swapi-api.hbtn.io/api/films/${movieId}`, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  
+  const movie = JSON.parse(body);
+  
+  console.log(movie.title);
+});
